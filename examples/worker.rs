41f09ba8ce6fdbad10146b18740997830edb1298
Jan-Erik Rediger <janerik@fnordig.de>
2016-02-14 18:03:27 +0100

    Implement example worker

---
+++ examples/worker.rs
@@ -0,0 +1,26 @@
+extern crate rustc_serialize;
+extern crate redis;
+extern crate oppgave;
+
+use oppgave::Worker;
+
+#[derive(RustcDecodable, RustcEncodable, Debug)]
+struct Job {
+    id: u64
+}
+
+fn main() {
+    let client = redis::Client::open("redis://127.0.0.1/").unwrap();
+    let con = client.get_connection().unwrap();
+    let worker = Worker::new("default".into(), con);
+
+    println!("Starting worker with queue `default`");
+
+    while let Some(task) = worker.next::<Job>() {
+        if task.is_err() { continue; }
+
+        let task = task.unwrap();
+
+        println!("Task: {:?}", task.inner());
+    }
+}
